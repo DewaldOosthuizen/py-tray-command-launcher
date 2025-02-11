03f714f5eb7703068616298a2035f1596d93b6c1
Dewald Oosthuizen <dewald.oosthuizen1@gmail.com>
2025-02-11 13:59:42 +0200

    Update path logic

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 import json
 import subprocess
+import os
 from PyQt6.QtWidgets import QApplication, QMenu, QSystemTrayIcon
 from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import QCoreApplication
 
-CONFIG_FILE = "commands.json"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_FILE = os.path.join(BASE_DIR, "commands.json")
+ICON_FILE = os.path.join(BASE_DIR, "icon.png")
 
 def load_commands():
     """Load commands from the JSON configuration file."""
@@ -21,11 +25,16 @@ def execute_command(command):
 class TrayApp:
     def __init__(self):
         self.app = QApplication(sys.argv)
-        self.tray_icon = QSystemTrayIcon(QIcon("icon.png"))
+        self.app.aboutToQuit.connect(self.cleanup)
+        self.tray_icon = QSystemTrayIcon(QIcon(ICON_FILE))
         self.menu = QMenu()
         self.load_tray_menu()
         self.tray_icon.setContextMenu(self.menu)
         self.tray_icon.show()
+
+    def cleanup(self):
+        """Perform any cleanup before quitting."""
+        print("Cleaning up before exit...")
 
     def load_tray_menu(self):
         """Load commands into the tray menu."""
@@ -37,6 +46,13 @@ class TrayApp:
             self.menu.addMenu(submenu)
         self.menu.addSeparator()
         self.menu.addAction("Exit", self.app.quit)
+        self.menu.addAction("Force Quit", lambda: self.force_quit())
+
+    def force_quit(self):
+        """Force quit the application."""
+        self.cleanup()
+        sys.exit()
+        QCoreApplication.exit()
 
     def run(self):
         sys.exit(self.app.exec())
